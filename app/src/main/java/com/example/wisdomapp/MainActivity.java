b052deec837f4cc69d5ab47fdb684dd8f9d6d8ef
irodrigon <75447146+irodrigon@users.noreply.github.com>
2024-11-05 17:57:09 +0100

    First commit with real functionality and intents, can go to videos activity but there is nothing on it yet.

--- app/src/main/java/com/example/wisdomapp/MainActivity.java
+++ app/src/main/java/com/example/wisdomapp/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.wisdomapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.gridlayout.widget.GridLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +16,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private GridLayout mainButtonImages;
+    private GridLayout mainButtonVideos;
+    private GridLayout mainButtonAudios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        mainButtonImages = (GridLayout) findViewById(R.id.mainButtonImages);
+        mainButtonVideos = (GridLayout) findViewById(R.id.mainButtonVideos);
+        mainButtonAudios = (GridLayout) findViewById(R.id.mainButtonAudios);
+
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Intent data = result.getData();
+                        }
+                    }
+                }
+        );
+
+        mainButtonVideos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, VideoLibraryActivity.class);
+                activityResultLauncher.launch(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
